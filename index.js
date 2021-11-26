57f6b4b97a3d4c2dc2eee1677396be5ceb2e3f2d
Adrian <adrianjperez@lewisu.edu>
2021-11-26 10:44:59 -0600

    added  cros

--- index.js
+++ index.js
@@ -1,61 +1,58 @@
 // https://github.com/WebDevSimplified/Realtime-Simple-Chat-App/blob/master/script.js
 const express = require("express");
-const { createServer } = require("http");
-const { dirname } = require("path");
-const { Server } = require("socket.io");
-
 const app = express();
-const httpServer = createServer(app);
-const io = new Server(httpServer, {  
-    cors: {
-        origin: true
-    }
-});
-const port = process.env.PORT || 3030;
+const cors = require("cors")
+app.use(
+  cors({
+    origin: "*"
+  })
+)
 
+const port = process.env.PORT || 3030;
+const server = app.listen(port)
 app.use(express.static(__dirname + 'node_modules'))
 
-app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/index.html');
-});
+console.log("server is running") //link to test http://localhost:3030/socket.io/socket.io.js
 
-//app.use('/', express.static('node_modules'));
+// const { createServer } = require("http");
+// const { dirname } = require("path");
+// const { Server } = require("socket.io");
+
+const socket = require('socket.io')
+//const httpServer = createServer(app);
+const io = socket(server)
 
 
 
-//to test when running  http://localhost:3030/socket.io/socket.io.js
-// const io = require('socket.io')( 3030,{
-//     cors: {
-//       origin: true,
-//     //   methods: ["GET", "POST"]
-//     },
-//   })
-  
- ///thing for the socker server 
-  const users = {}
-  io.on('connection', (socket) => {
-    socket.on('new-user', username => {
-      users[socket.id] = username
-      socket.broadcast.emit('user-connected', username)
-    })
-    console.log('a user connected');
-    //socket.emit('chat-message', 'hello word')//sending test to users
-  
-    socket.on('send-chat-message', message => {
-      //console.log(message)
-      socket.broadcast.emit('chat-message', { message: message, username: users[socket.id] })
-    })
-  
-    socket.on('disconnect', () => {
-      //console.log('user disconnected');
-      socket.broadcast.emit('user-disconnected', users[socket.id])
-      delete users[socket.id]
-  
-    });
+// app.get('/', function(req, res) {
+//   res.sendFile(__dirname + '/index.html');
+// });
+
+
+///thing for the socker server 
+const users = {}
+io.on('connection', (socket) => {
+  socket.on('new-user', username => {
+    users[socket.id] = username
+    socket.broadcast.emit('user-connected', username)
   })
-  // this is the end of io.on
- 
-  
-  //io.listen(3030);
+  console.log('a user connected');
+  //socket.emit('chat-message', 'hello word')//sending test to users
 
-  httpServer.listen(port);
+  socket.on('send-chat-message', message => {
+    //console.log(message)
+    socket.broadcast.emit('chat-message', { message: message, username: users[socket.id] })
+  })
+
+  socket.on('disconnect', () => {
+    //console.log('user disconnected');
+    socket.broadcast.emit('user-disconnected', users[socket.id])
+    delete users[socket.id]
+
+  });
+})
+// this is the end of io.on
+
+
+
+//httpServer.listen(port);
